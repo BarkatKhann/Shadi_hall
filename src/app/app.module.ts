e25eafd9cd05e1637e7c9363e434f3dd9add51fe
BarkatKhann <mdbarkatali5@gmail.com>
2025-06-08 17:04:23 +0530

    Merge branch 'main' of https://github.com/BarkatKhann/Shadi_hall

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './interceptors/auth.interceptor';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,7 +20,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     FormsModule,
     ReactiveFormsModule, 
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideHttpClient(withInterceptors([authInterceptor]))
+    // { provide: HTTP_INTERCEPTORS, useClass: authInterceptor,multi:true }
+
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
